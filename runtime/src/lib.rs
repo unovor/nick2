d84fcbcf9cce62057e311ec34eb2b5df4b6abc74
unovor <15306517+unovor@users.noreply.github.com>
2020-11-19 04:12:35 -0800

    3

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -245,6 +245,40 @@ impl pallet_balances::Trait for Runtime {
 }
 
 parameter_types! {
+    // Choose a fee that incentivizes desireable behavior.
+    pub const NickReservationFee: u128 = 100;
+    pub const MinNickLength: usize = 8;
+    // Maximum bounds on storage are important to secure your chain.
+    pub const MaxNickLength: usize = 32;
+}
+
+impl pallet_nicks::Trait for Runtime {
+    // The Balances pallet implements the ReservableCurrency trait.
+    // https://substrate.dev/rustdocs/v2.0.0/pallet_balances/index.html#implementations-2
+	type Currency = pallet_balances::Module<Runtime>;
+	
+    // Use the NickReservationFee from the parameter_types block.
+	type ReservationFee = NickReservationFee;
+	
+    // No action is taken when deposits are forfeited.
+    type Slashed = ();
+
+    // Configure the FRAME System Root origin as the Nick pallet admin.
+    // https://substrate.dev/rustdocs/v2.0.0/frame_system/enum.RawOrigin.html#variant.Root
+    type ForceOrigin = frame_system::EnsureRoot<AccountId>;
+
+    // Use the MinNickLength from the parameter_types block.
+    type MinLength = MinNickLength;
+
+    // Use the MaxNickLength from the parameter_types block.
+    type MaxLength = MaxNickLength;
+
+    // The ubiquitous event type.
+	type Event = Event;
+}
+
+
+parameter_types! {
 	pub const TransactionByteFee: Balance = 1;
 }
 
@@ -283,6 +317,7 @@ construct_runtime!(
 		Sudo: pallet_sudo::{Module, Call, Config<T>, Storage, Event<T>},
 		// Include the custom logic from the template pallet in the runtime.
 		TemplateModule: pallet_template::{Module, Call, Storage, Event<T>},
+		Nicks: pallet_micls::{Module, Call, Storage, Event<T>},
 	}
 );
 
